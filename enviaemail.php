2f832314fc2fa1d1dd3918d9da1baab23194660e
Jean Mayer <jean.mayer@pisco.net.br>
2018-03-26 09:16:13 -0300

    envia

--- enviaemail.php
+++ enviaemail.php
@@ -8,38 +8,38 @@
     $mensagem = $_POST["mensagem"];
     $email = $_POST["email"];
 
-	function EnviaEmail($nomeDestinatario, $emailDestinatario, $assunto, $mensagem, $email){
-        $mail = new PHPMailer();
+    $mail = new PHPMailer();
 
-        $mail->SMTPOptions = array(
-            'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-            )
-        );
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+        )
+    );
 
-        $mail->IsSMTP();
-        $mail->Charset = "UTF-8";
-        $mail->ContentType = "Content-Type: text/html";
-        $mail->Encoding = "base64";
-        $mail->SMTPAuth = true;
+    $mail->IsSMTP();
+    $mail->Charset = "UTF-8";
+    $mail->ContentType = "Content-Type: text/html";
+    $mail->Encoding = "base64";
+    $mail->SMTPAuth = true;
 
-        $mail->SMTPSecure = "ssl";
+    $mail->SMTPSecure = "ssl";
 
-        $mail->Host = "smtp.pisco.net.br";
-        $mail->Port = 465;
-        $mail->Username = $email;
-        $mail->Password = "3edsw21qa";
-        $mail->SetFrom($email, "PISCO SOFTWARE");
+    $mail->Host = "smtp.pisco.net.br";
+    $mail->Port = 465;
+    $mail->Username = $email;
+    $mail->Password = "3edsw21qa";
+    $mail->SetFrom($email, "PISCO SOFTWARE");
 
-        $mail->AddAddress($emailDestinatario, $nomeDestinatario);
-        $mail->IsHTML(true);
+    $mail->AddAddress($emailDestinatario, $nomeDestinatario);
+    $mail->IsHTML(true);
 
-        $mail->Subject = $assunto;
-        $mail->MsgHTML($mensagem);
-        if($mail->Send()) {
-            return 1;
-        } else {
-            return 0;
-        }
+    $mail->Subject = $assunto;
+    $mail->MsgHTML($mensagem);
+
+    if($mail->Send()) {
+        return 1;
+    } else {
+        return 0;
+    }
